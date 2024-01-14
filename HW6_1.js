b3ea3548dd773e1f367d3a0304186d7e26106902
Vladyslav-Litvinov <152916799+Vladyslav-Litvinov@users.noreply.github.com>
2024-01-14 18:43:39 +0200

    Add files via upload

--- HW6_1.js
+++ HW6_1.js
@@ -1,10 +1,20 @@
-function drawTriagle(height, symbol) {
-    for (let i = 1; i <= height; i++) {
-        let line = '';
-        for (let j = 1; j <= i; j++) {
-            line += symbol;
-        }
-        console.log(line);
+// Варіант 1
+let height = 5;
+let symbol = '*';
+for(let i = 1; i <= height; i++) 
+{
+    let line = '';
+    for(let j = 0; j < i; j++) 
+    {
+        line += symbol;
     }
+    console.log(line);
 }
-drawTriagle(5, '*');
+
+// Варіант 2
+let height = 5;
+let symbol = '*';
+
+for(let i = 1; i <= height; i++) {
+    console.log(symbol.repeat(i));
+}
